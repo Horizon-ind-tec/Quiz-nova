627431e31a8dbd0512de6aab42a0a3f07f8de45e
WizofclassKnowledge <wizofclassknowledge33@gmail.com>
2026-01-12 18:36:58 +0000

    Please reload the app

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,14 @@
-
 import { SideNav } from '@/components/side-nav';
 import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-      <SidebarProvider>
-        <div className="flex min-h-screen bg-background">
-          <SideNav />
-          <main className="flex-1 min-w-0">{children}</main>
-        </div>
-      </SidebarProvider>
+    <SidebarProvider>
+      <div className="flex min-h-screen bg-background">
+        <SideNav />
+        {/* The main content area for the application */}
+        <main className="flex-1 min-w-0">{children}</main>
+      </div>
+    </SidebarProvider>
   );
 }
