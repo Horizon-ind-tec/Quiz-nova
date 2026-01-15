5da112aada0ec29fc54c23f297ff5ba4d1aea606
WizofclassKnowledge <wizofclassknowledge33@gmail.com>
2026-01-15 05:37:47 +0000

    Install Capacitor: Run these commands in your project terminal: npm inst

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.quiznova.app',
+  appName: 'QuizNova',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
