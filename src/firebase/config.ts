837e656de3547bf980bf4bdc3e18e49701ed8284
WizofclassKnowledge <wizofclassknowledge33@gmail.com>
2026-01-16 14:34:22 +0000

    AIzaSyA6i0rDCwTal_bmzeYwNZ5oMfr3vyEJog8 here is a new API ki please impl

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-770359382-db111",
   "appId": "1:79648401698:web:31030f0df29820dbe2dd7b",
-  "apiKey": "AIzaSyASjQkpCYnbsS_csgCdWbnrWk27Q9m7sOo",
+  "apiKey": "AIzaSyA6i0rDCwTal_bmzeYwNZ5oMfr3vyEJog8",
   "authDomain": "studio-770359382-db111.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "79648401698"
